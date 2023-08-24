8df7c9f38ff5c0538fa004e88f314a271c3af900
Dean Lofts <dean@deanlofts.xyz>
2023-08-25 01:00:33 +1000

    replace webrick

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'google_drive'
+gem 'sinatra'
--- lib/drivegit/drive_module.rb
+++ lib/drivegit/drive_module.rb
@@ -1,52 +1,49 @@
+require 'sinatra'
 require 'google_drive'
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
-require 'webrick'
 
 module DriveGit
   module DriveModule
     def self.authenticate
-  config_path = File.expand_path("../../../credentials.json", __FILE__)
-  client_id = Google::Auth::ClientId.from_file(config_path)
-  scope = ["https://www.googleapis.com/auth/drive"]
-  
-  token_store_path = File.expand_path("../../../token.yaml", __FILE__)
-  token_store = Google::Auth::Stores::FileTokenStore.new(file: token_store_path)
+      config_path = File.expand_path("../../../credentials.json", __FILE__)
+      client_id = Google::Auth::ClientId.from_file(config_path)
+      scope = ["https://www.googleapis.com/auth/drive"]
 
-  authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
-  user_id = 'default'
-  credentials = authorizer.get_credentials(user_id)
+      token_store_path = File.expand_path("../../../token.yaml", __FILE__)
+      token_store = Google::Auth::Stores::FileTokenStore.new(file: token_store_path)
 
-  if credentials.nil?
-    server = WEBrick::HTTPServer.new(:Port => 8000)
-    code = nil
+      authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
+      user_id = 'default'
+      credentials = authorizer.get_credentials(user_id)
 
-    server.mount_proc '/' do |req, res|
-      code = req.query['code']
-      res.body = 'Code received. You may close this page.'
-      server.shutdown
+      if credentials.nil?
+        code = nil
+
+        get '/' do
+          code = params['code']
+          'Code received. You may close this page.'
+          # Here you might want to handle the code and shut down Sinatra, depending on your needs
+        end
+
+        url = authorizer.get_authorization_url(
+          base_url: 'http://localhost:4567'
+        )
+
+        puts "Open the following URL in the browser and enter the resulting code after authorization:\n" + url
+
+        sleep 1 while code.nil?
+
+        credentials = authorizer.get_and_store_credentials_from_code(
+          user_id: user_id, code: code, base_url: 'http://localhost:4567'
+        )
+      end
+
+      raise "No credentials available" unless credentials
+      session = GoogleDrive::Session.from_credentials(credentials)
+
+      session
     end
-
-    url = authorizer.get_authorization_url(
-      base_url: 'http://localhost:8000'
-    )
-    
-    puts "Open the following URL in the browser and enter the resulting code after authorization:\n" + url
-
-    Thread.new { server.start }
-    sleep 1 while code.nil?
-
-    credentials = authorizer.get_and_store_credentials_from_code(
-      user_id: user_id, code: code, base_url: 'http://localhost:8000'
-    )
-  end
-
-  raise "No credentials available" unless credentials
-  session = GoogleDrive::Session.from_credentials(credentials)
-
-  session
-end
-
 
     def self.upload_file(filepath)
       session = authenticate
@@ -57,16 +54,16 @@ end
       session = authenticate
       file = session.file_by_title(filename)
       file_id = file.id
-    
+
       drive_service = session.drive_service
       dest_path = File.join(download_path, filename)
-    
+
       File.open(dest_path, 'wb') do |file_io|
         drive_service.get_file(file_id, download_dest: file_io)
       end
-    
+
       puts "File downloaded to #{dest_path}"
-    end       
+    end
 
     def self.list_files
       session = authenticate
