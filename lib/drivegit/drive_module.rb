751be817240c53501fa126d5fe57561aa25e6cbd
Dean Lofts <dean@deanlofts.xyz>
2023-08-24 22:19:57 +1000

    web server for callback

--- lib/drivegit/drive_module.rb
+++ lib/drivegit/drive_module.rb
@@ -1,37 +1,52 @@
 require 'google_drive'
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
+require 'webrick'
 
 module DriveGit
   module DriveModule
     def self.authenticate
-      config_path = File.expand_path("../../../credentials.json", __FILE__)
-      client_id = Google::Auth::ClientId.from_file(config_path)
-      scope = ["https://www.googleapis.com/auth/drive"]
+  config_path = File.expand_path("../../../credentials.json", __FILE__)
+  client_id = Google::Auth::ClientId.from_file(config_path)
+  scope = ["https://www.googleapis.com/auth/drive"]
+  
+  token_store_path = File.expand_path("../../../token.yaml", __FILE__)
+  token_store = Google::Auth::Stores::FileTokenStore.new(file: token_store_path)
+
+  authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
+  user_id = 'default'
+  credentials = authorizer.get_credentials(user_id)
+
+  if credentials.nil?
+    server = WEBrick::HTTPServer.new(:Port => 8000)
+    code = nil
+
+    server.mount_proc '/' do |req, res|
+      code = req.query['code']
+      res.body = 'Code received. You may close this page.'
+      server.shutdown
+    end
+
+    url = authorizer.get_authorization_url(
+      base_url: 'http://localhost:8000'
+    )
     
-      token_store_path = File.expand_path("../../../token.yaml", __FILE__)
-      token_store = Google::Auth::Stores::FileTokenStore.new(file: token_store_path)
-    
-      authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
-      user_id = 'default'
-      credentials = authorizer.get_credentials(user_id)
-    
-      if credentials.nil?
-        url = authorizer.get_authorization_url(
-          base_url: 'http://localhost'
-        )
-        puts "Open the following URL in the browser and enter the resulting code after authorization:\n" + url
-        code = $stdin.gets.chomp # Change this line
-        credentials = authorizer.get_and_store_credentials_from_code(
-          user_id: user_id, code: code, base_url: 'http://localhost'
-        )
-      end      
-    
-      raise "No credentials available" unless credentials
-      session = GoogleDrive::Session.from_credentials(credentials)
-    
-      session
-    end    
+    puts "Open the following URL in the browser and enter the resulting code after authorization:\n" + url
+
+    Thread.new { server.start }
+    sleep 1 while code.nil?
+
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: 'http://localhost:8000'
+    )
+  end
+
+  raise "No credentials available" unless credentials
+  session = GoogleDrive::Session.from_credentials(credentials)
+
+  session
+end
+
 
     def self.upload_file(filepath)
       session = authenticate
