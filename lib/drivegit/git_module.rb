38e8406774b54b1e51468cc01621857b999477d7
Dean Lofts <dean@deanlofts.xyz>
2023-08-27 14:00:47 +1000

    add git commands and git module

--- lib/drivegit/git_module.rb
+++ lib/drivegit/git_module.rb
@@ -1,8 +1,23 @@
 module DriveGit
-    module GitModule
-      def self.info
-        puts "GitModule: Handling Git operations!"
-      end
+  module GitModule
+    def self.init
+      puts "Initializing new Git repo!"
+      # Run git init commands here
+    end
+
+    def self.clone
+      puts "Cloning Git repo from Drive!"
+      # Clone commands here
+    end
+
+    def self.push
+      puts "Pushing changes to Drive!"
+      # Push commands here
+    end
+
+    def self.pull
+      puts "Pulling changes from Drive!"
+      # Pull commands here
     end
   end
-  
+end
